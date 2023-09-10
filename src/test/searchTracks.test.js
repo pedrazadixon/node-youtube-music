7081b5a28204129970a0a1c9fdd70505d67840ba
Dixon Pedraza <pedrazadixon@gmail.com>
2023-09-09 20:59:48 -0500

    improve searchTracks.test

--- src/test/searchTracks.test.js
+++ src/test/searchTracks.test.js
@@ -9,8 +9,18 @@ test('test if searchTracks find tracks', async () => {
     const query = "hello"
     searchResult = await searchTracks(query);
     expect(searchResult.tracks.length).toBeGreaterThan(0)
-    track2 = searchResult.tracks[0]
+    track1 = searchResult.tracks[0]
 })
+
+test('test if track has required properties', async () => {
+    expect(track1).toHaveProperty('trackId')
+    expect(track1).toHaveProperty('title')
+    expect(track1).toHaveProperty('artists')
+    expect(track1).toHaveProperty('album')
+    expect(track1).toHaveProperty('thumbnailUrl')
+    expect(track1).toHaveProperty('duration')
+})
+
 
 test('test searchTracks continuations', async () => {
     expect(searchResult).toHaveProperty('continuation')
