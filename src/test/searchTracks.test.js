9909ce112d17374b96e18f5fdeb9a6caefa5df50
Dixon Pedraza <pedrazadixon@gmail.com>
2023-09-09 21:05:21 -0500

    improve test searchTracks

--- src/test/searchTracks.test.js
+++ src/test/searchTracks.test.js
@@ -5,24 +5,31 @@ let searchResult;
 let track1 = {}
 let track2 = {}
 
-test('test if searchTracks find tracks', async () => {
+test('searchTracks: test if find tracks', async () => {
     const query = "hello"
     searchResult = await searchTracks(query);
     expect(searchResult.tracks.length).toBeGreaterThan(0)
     track1 = searchResult.tracks[0]
 })
 
-test('test if track has required properties', async () => {
+test('searchTracks: test if track has correct required properties', async () => {
     expect(track1).toHaveProperty('trackId')
     expect(track1).toHaveProperty('title')
     expect(track1).toHaveProperty('artists')
     expect(track1).toHaveProperty('album')
     expect(track1).toHaveProperty('thumbnailUrl')
     expect(track1).toHaveProperty('duration')
+
+    expect(track1.trackId).toBeTypeOf('string')
+    expect(track1.title).toBeTypeOf('string')
+    expect(track1.artists).toBeTypeOf('object')
+    expect(track1.album).toBeTypeOf('object')
+    expect(track1.thumbnailUrl).toBeTypeOf('string')
+    expect(track1.duration).toBeTypeOf('object')
 })
 
 
-test('test searchTracks continuations', async () => {
+test('searchTracks: test searchTracks continuations', async () => {
     expect(searchResult).toHaveProperty('continuation')
     let continuations = await searchTracksContinuations(searchResult.continuation);
     expect(continuations.tracks.length).toBeGreaterThan(0)
